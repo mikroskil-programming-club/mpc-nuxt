ea7be6023111cee658fcc795b7ddad8953a011fe
Sam! <156488637+sammmms@users.noreply.github.com>
2024-02-01 22:23:09 +0700

    haish route nya ternyata bikin error and crash di netlify, jadi balikin ke yang seharusnya aja la,, so yeah commit ulang

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -3,11 +3,9 @@ import axios from "axios"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const store = useStore()
-    if(to.path == "/register" && (!from.path=="/members" || !from.path == "/material") && store.isAuthenticated) return navigateTo('/')
-    if(to.path == "/login" && (!from.path=="/members" || !from.path == "/material") && store.isAuthenticated) return navigateTo('/')
-    if(to.path == "/members"){ axios.post('/api/users/verify',{token:store.token}).catch(()=>{return navigateTo('/login')})}
+    if(to.path == "/register" && store.isAuthenticated) return navigateTo('/')
+    if(to.path == "/login"&& store.isAuthenticated) return navigateTo('/')
     if(to.path == "/members" && !store.isAuthenticated && !store.isAdmin) {return navigateTo('/login')}
-    if((to.path == "/material" || to.path == "/material/coach" || to.path == "/material/video")){ axios.post('/api/users/verify',{token:store.token}).catch(()=>{return navigateTo('/login')})}
     if((to.path == "/material" || to.path == "/material/coach" || to.path == "/material/video") && !store.isAuthenticated) {return navigateTo('/login')}
   })
   
