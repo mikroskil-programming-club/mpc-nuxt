b6ffb3bb40fc41a3e0c0264cf63a0b065804624b
Sam! <156488637+sammmms@users.noreply.github.com>
2024-02-03 12:33:21 +0700

    added route guard to /manage/**

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -5,8 +5,14 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const store = useStore()
     if(to.path == "/register" && store.isAuthenticated) return navigateTo('/')
     if(to.path == "/login" && store.isAuthenticated) return navigateTo('/')
-    if(to.path == "/events" && (!store.isAuthenticated || !store.isAdmin)) return navigateTo('/')
+    if((to.path == "/manage/events" ||
+    to.path == "/manage/material" ||
+    to.path == "/manage/schedule" ) && (!store.isAuthenticated || !store.isAdmin)) 
+    return navigateTo('/')
     if(to.path == "/members" && !store.isAuthenticated) {return navigateTo('/login')}
-    if((to.path == "/material" || to.path == "/material/coach" || to.path == "/material/video") && !store.isAuthenticated) {return navigateTo('/login')}
+    if((to.path == "/material" || 
+    to.path == "/material/coach" || 
+    to.path == "/material/video") 
+    && !store.isAuthenticated) {return navigateTo('/login')}
   })
   
