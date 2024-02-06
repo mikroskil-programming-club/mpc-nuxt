0a9b24557956b72e3e6ade1dba898148773341a7
Tristan <29161072+trisbman@users.noreply.github.com>
2024-02-06 16:12:47 +0700

    feat: netlify form integration

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,5 +1,7 @@
 <script setup>
 import contactCard from '~/components/contact-card.vue';
+const responseMessage = ref('')
+const showAlert = ref(false)
 import { ref } from 'vue'
 
 const NIM = ref('')
@@ -16,8 +18,8 @@ const handleEmail = (e) => {
     email.value = e.target.value
 }
 
-const handleSubmit = ()=>{
-    console.log(NIM.value, problem.value)
+const handleSubmit = (e) => {
+    // netlify will show successful submission page
 }
 
 </script>
@@ -26,12 +28,17 @@ const handleSubmit = ()=>{
         <div class="flex justify-center py-10 pt-0 font-semibold text-2xl">Contact Us</div>
         <div class="flex flex-col items-center justify-center">
             <div class="flex justify-center py-10 font-semibold text-2xl">Form Submission</div>
-            <div class="w-[300px] mx-auto md:w-[800px] flex flex-col gap-4">
-                <input @change="handleNIM" class="p-4 rounded-xl shadow-lg" placeholder="NIM" type="text">
-                <input @change="handleEmail" class="p-4 rounded-xl shadow-lg" placeholder="Email" type="text">
-                <textarea @change="handleProblem" class="p-4 rounded-xl shadow-lg text-start h-[200px]" placeholder="Problems" type="text"/>  
-                <button @click="handleSubmit" class="mx-auto bg-blue-300 w-40 p-2 rounded-xl hover:bg-blue-400 shadow-lg">Submit</button>
-            </div>
+            <form @submit="handleSubmit" name="contact" method="POST"
+                class="w-[300px] mx-auto md:w-[800px] flex flex-col gap-4" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
+                <input @change="handleNIM" class="p-4 rounded-xl shadow-lg" placeholder="NIM" type="text" name="NIM" required />
+                <input @change="handleEmail" class="p-4 rounded-xl shadow-lg" placeholder="Email" type="email"
+                    name="email" required />
+                <textarea @change="handleProblem" class="p-4 rounded-xl shadow-lg text-start h-[200px]"
+                    placeholder="Problems" type="text" name="problem" required />
+                <button type="submit"
+                    class="mx-auto bg-blue-300 w-40 p-2 rounded-xl hover:bg-blue-400 shadow-lg">Submit</button>
+            </form>
         </div>
     </div>
 </template>
