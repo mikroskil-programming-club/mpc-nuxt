07dda9225cea16b382452f924232da8eb0ab6fd4
Tristan <29161072+trisbman@users.noreply.github.com>
2024-02-07 11:39:39 +0700

    other: remove unnecessary handleSubmit

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -28,7 +28,7 @@ const handleSubmit = (e) => {
         <div class="flex justify-center py-10 pt-0 font-semibold text-2xl">Contact Us</div>
         <div class="flex flex-col items-center justify-center">
             <div class="flex justify-center py-10 font-semibold text-2xl">Form Submission</div>
-            <form @submit="handleSubmit" name="contact" method="POST"
+            <form name="contact" method="POST"
                 class="w-[300px] mx-auto md:w-[800px] flex flex-col gap-4" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact" />
                 <input @change="handleNIM" class="p-4 rounded-xl shadow-lg" placeholder="NIM" type="text" name="NIM" required />
